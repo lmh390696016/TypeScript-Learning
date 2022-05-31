27f3d74e05f5008c55c6915366a312fc876f53c9
LiMinghao <390696016@qq.com>
2022-05-31 16:28:07 +0800

    对象静态类型

---
+++ TypeScript-Learning/Demo3.ts
@@ -0,0 +1,26 @@
+const count: number = 918;
+const myName: string = 'liminghao'
+// null, undefined, boolean, void, symbol
+
+// 最普通的对象类型  
+const human: {
+    name: string,
+    age: number
+} = {
+    name: 'Ash',
+    age: 18
+}
+
+// 数组类型
+const humans: string[] = ['张三','李四','王五']
+
+// 类类型
+class Person{}
+const zhangsan : Person = new Person()
+
+// 函数类型
+const meet : () => string = ()=> {
+    return '张三'
+}
+
+// 对象类型 数组类型 类类型 函数类型
