f58186b752d7376a309bdc68bccc82acd851e898
LiMinghao <390696016@qq.com>
2022-05-31 15:59:58 +0800

    静态类型

---
+++ TypeScript-Learning/Demo2.ts
@@ -0,0 +1,13 @@
+let count: number = 1;
+
+// 自定义静态类型
+interface Li {
+    uname: String,
+    age: number
+}
+
+const xiaohong : Li = {
+    uname: '小红',
+    age: 18
+}
+console.log(xiaohong.age);
