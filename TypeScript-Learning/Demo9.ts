460112a5568a726b892c9367052bf4764b7da23c
LiMinghao <390696016@qq.com>
2022-06-01 09:16:22 +0800

    类的概念和使用

---
+++ TypeScript-Learning/Demo9.ts
@@ -0,0 +1,21 @@
+class Person {
+    content = 'Hi';
+    sayHello() {
+        return this.content
+    }
+}
+
+class Human extends Person {
+    sayHello() {
+        return super.sayHello() + '.你好！'
+    }
+
+    sayLove() {
+        return 'I Love You';
+    }
+}
+
+const goddess = new Human()
+console.log(goddess.sayHello());
+console.log(goddess.sayLove());
+
